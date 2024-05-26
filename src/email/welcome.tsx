4e84de1945f5738f58b5fdc915be5446f490f046
Evan <mr.evan.gordon@gmail.com>
2024-05-26 23:14:00 +0100

    added image to react email

--- src/email/welcome.tsx
+++ src/email/welcome.tsx
@@ -1,20 +1,102 @@
-import { Html, Heading, Text } from "@react-email/components"
+
+import {
+  Body,
+  Button,
+  Container,
+  Head,
+  Hr,
+  Html,
+  Img,
+  Preview,
+  Section,
+  Text,
+} from "@react-email/components";
+import * as React from "react";
+
 const EmailTemplate = ({
   name,
-  email,
-
 }: {
-  name: string
-  email: string
-}) => {
-  return (
-    <Html lang="en">
-      <Heading as="h1">Welcome to the Club!</Heading>
-      <Text>You have just joined the Mentors club!</Text>
-      <Text>Name: {name}</Text>
-      <Text>Email: {email}</Text>
-    
-    </Html>
-  )
-}
-export default EmailTemplate
+  name: string,
+}) => 
+
+  <Html>
+    <Head />
+    <Preview>
+      The sales intelligence platform that helps you uncover qualified leads.
+    </Preview>
+    <Body style={main}>
+      <Container style={container}>
+        <Img
+         src={`/static/mentor.png`}
+          width="140"
+          height="160"
+          alt="Koala"
+        
+        />
+        <Text style={paragraph}>Hi {name},</Text>
+        <Text style={paragraph}>
+          Welcome to Mentors Club
+        </Text>
+        <Section style={btnContainer}>
+          {/* <Button style={button} href="https://getkoala.com">
+            Get started
+          </Button> */}
+        </Section>
+        <Text style={paragraph}>
+          Best,
+          <br />
+          Evan from The Mentors Club
+        </Text>
+        <Hr style={hr} />
+        <Text style={footer}>
+          Dublin 9
+        </Text>
+      </Container>
+    </Body>
+  </Html>
+
+
+
+export default EmailTemplate;
+
+const main = {
+  backgroundColor: "#ffffff",
+  fontFamily:
+    '-apple-system,BlinkMacSystemFont,"Segoe UI",Roboto,Oxygen-Sans,Ubuntu,Cantarell,"Helvetica Neue",sans-serif',
+};
+
+const container = {
+  margin: "0 auto",
+  padding: "20px 0 48px",
+};
+
+
+const paragraph = {
+  fontSize: "16px",
+  lineHeight: "26px",
+};
+
+const btnContainer = {
+  textAlign: "center" as const,
+};
+
+const button = {
+  backgroundColor: "#5F51E8",
+  borderRadius: "3px",
+  color: "#fff",
+  fontSize: "16px",
+  textDecoration: "none",
+  textAlign: "center" as const,
+  display: "block",
+  padding: "12px",
+};
+
+const hr = {
+  borderColor: "#cccccc",
+  margin: "20px 0",
+};
+
+const footer = {
+  color: "#8898aa",
+  fontSize: "12px",
+};

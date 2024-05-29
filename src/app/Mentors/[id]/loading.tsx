3baa43c56169289aada880f2008c1e5bdf254625
Evan <mr.evan.gordon@gmail.com>
2024-05-29 21:22:16 +0100

    testing

--- src/app/Mentors/[id]/loading.tsx
+++ src/app/Mentors/[id]/loading.tsx
@@ -1,17 +1,110 @@
-import { Skeleton, SkeletonCircle, SkeletonText, Stack } from '@chakra-ui/react'
-import React from 'react'
+import React from 'react';
+import {
+  Box,
+  Heading,
+  Text,
+  Tag,
+  Flex,
+  Container,
+  Stack,
+  VStack,
+  Skeleton, SkeletonCircle, SkeletonText
+} from '@chakra-ui/react';
 
 
 
-
-export default function Loading() {
-
-
+const Loading = () => {
   return (
-    <Stack>
-    <Skeleton height='20px' />
-    <Skeleton height='20px' />
-    <Skeleton height='20px' />
-  </Stack>
-  )
-}
+    <Container maxW={'7xl'} p="12">
+      <Heading as="h1"> 
+      <Skeleton startColor='green.500' height='20px' />
+      </Heading>
+      
+      <Box
+        marginTop={{ base: '1', sm: '5' }}
+        display="flex"
+        flexDirection={{ base: 'column', sm: 'row' }}
+        justifyContent="space-between">
+        <Box
+          display="flex"
+          flex="1"
+          marginRight="3"
+          position="relative"
+          alignItems="center">
+          <Box
+            width={{ base: '100%', sm: '85%' }}
+            zIndex="2"
+            marginLeft={{ base: '0', sm: '5%' }}
+            marginTop="5%">
+              
+ <div className='corners2'>
+ <Skeleton
+        height='60px'
+     
+        fadeDuration={4}
+      
+        color='white'
+      />
+              </div>
+  
+          </Box>
+          <Box zIndex="1" width="100%" position="absolute" height="100%">
+            <Box
+        
+              backgroundSize="20px 20px"
+              opacity="0.4"
+              height="100%"
+            />
+          </Box>
+        </Box>
+        
+        
+        <Box
+          display="flex"
+          flex="1"
+          flexDirection="column"
+          justifyContent="center"
+          marginTop={{ base: '3', sm: '0' }}>
+        
+       
+         <Flex>
+            <Heading textDecoration="none" _hover={{ textDecoration: 'none' }}>
+            <Skeleton startColor='green.500' height='20px' />
+            </Heading>
+         
+            <Tag  m={2} variant='solid'  colorScheme='green'>
+        
+    </Tag>
+    </Flex>
+          <Text
+            as="p"
+            marginTop="2"
+          
+            fontSize="lg">
+            <Stack>
+  <Skeleton height='20px' />
+  <Skeleton height='20px' />
+  <Skeleton height='20px' />
+</Stack>
+          </Text>
+       
+        </Box>
+      </Box>
+     
+      <VStack paddingTop="40px" spacing="2" alignItems="flex-start">
+        <Heading as="h2">Mentoring Topics</Heading>
+        <Text as="p" fontSize="lg">
+        <Stack>
+  <Skeleton height='20px' />
+  <Skeleton height='20px' />
+  <Skeleton height='20px' />
+</Stack>
+    
+        </Text>
+    
+      </VStack>
+    </Container>
+  );
+};
+
+export default Loading;

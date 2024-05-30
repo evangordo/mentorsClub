7d8517b6ac09240b3bfdeba0d8635e42d9328ef8
Evan <mr.evan.gordon@gmail.com>
2024-05-30 11:23:16 +0100

    fixes

--- src/app/Mentors/page.tsx
+++ src/app/Mentors/page.tsx
@@ -5,8 +5,8 @@ import { Container, Heading, SimpleGrid } from '@chakra-ui/react'
 import Card from '../components/Card'
 import Loading from './loading'
 
-const MentorsPage =  ()  => {
-// const mentors = await getAllMentors()
+const MentorsPage = async ()  => {
+const mentors = await getAllMentors()
 
 
 
@@ -16,12 +16,13 @@ const MentorsPage =  ()  => {
     <Container mt={8}maxW={'9xl'}>
       <Suspense  fallback={<Loading/>}>
     <SimpleGrid  spacing={4}columns ={[1,1,5]}>
-      {/* {mentors.map((mentor) => (
+      {mentors.map((mentor) => (
 <div key={mentor.id}>
 <Card mentor={mentor}/>
 </div>
-      ))} */}
-      <Heading>Testing</Heading>
+      ))}
+     
+
     </SimpleGrid>
     </Suspense>
    </Container>

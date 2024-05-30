3ef72659fc0063ff20667650722dff7c2a9f57d1
Evan <mr.evan.gordon@gmail.com>
2024-05-30 11:02:09 +0100

    testing mentors page

--- src/app/Mentors/page.tsx
+++ src/app/Mentors/page.tsx
@@ -1,7 +1,7 @@
 import React, { Suspense } from 'react'
 import SearchBar from '../components/Searchbar'
 import {getAllMentors} from '../lib/data'
-import { Container, SimpleGrid } from '@chakra-ui/react'
+import { Container, Heading, SimpleGrid } from '@chakra-ui/react'
 import Card from '../components/Card'
 import Loading from './loading'
 
@@ -9,18 +9,19 @@ const MentorsPage = async()  => {
 const mentors = await getAllMentors()
 
 
-console.log("getting:", mentors);
+
   return (
     <>
     <SearchBar/>
     <Container mt={8}maxW={'9xl'}>
       <Suspense  fallback={<Loading/>}>
     <SimpleGrid  spacing={4}columns ={[1,1,5]}>
-      {mentors.map((mentor) => (
+      {/* {mentors.map((mentor) => (
 <div key={mentor.id}>
 <Card mentor={mentor}/>
 </div>
-      ))}
+      ))} */}
+      <Heading>Testing</Heading>
     </SimpleGrid>
     </Suspense>
    </Container>
